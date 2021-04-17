30c4525560451bd48cc4af74bcf95a17b97c5254
patrick hrabos <phrabos@gmail.com>
2021-04-17 14:45:23 -0400

    DNA complements

--- src/kata.js
+++ src/kata.js
@@ -232,3 +232,30 @@ const multsOf3and5 = (number)=>{
   }
   return multsArr.reduce((acc, curr)=>acc + curr, 0);
 }
+
+// Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+// If you want to know more http://en.wikipedia.org/wiki/DNA
+
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". You have function with one side of the DNA (string, except for Haskell); you need to get the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+// More similar exercise are found here http://rosalind.info/problems/list-view/ (source)
+
+// DNAStrand ("ATTGC") // return "TAACG"
+
+// DNAStrand ("GTAT") // return "CATA" 
+
+function DNAStrnd(dna){
+  let result = '';
+
+  for (let i = 0; i < dna.length; i++) {
+    const strand = dna[i];
+    
+    if(strand === 'G') result += 'C'
+    if(strand === 'C') result += 'G'
+    if(strand === 'A') result += 'T'
+    if(strand === 'T') result += 'A'
+  }
+
+  return result;
+}
